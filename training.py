1dfceb98f5cd1db215f1de220f03712cab944e85
Franklin Simpson <simpson.franklin44@gmail.com>
2022-02-27 07:11:51 -0500

    git hub training

--- training.py
+++ training.py
@@ -1,2 +1,4 @@
 # Stsrt of python training series to teach at the youth center or inmates in some capacity.  Look into making a unicor style program where inmates that have life sentences can learn to code 
 # worrking remotely later on, so they can support their families remotely.
+
+Git/git hub training piece build out your own curriculum 

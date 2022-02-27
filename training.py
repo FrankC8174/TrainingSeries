f2019baaeb56bd6e89fc3f8599682e35dd9cd5d6
Franklin Simpson <simpson.franklin44@gmail.com>
2022-02-27 07:09:02 -0500

    Initial commit

---
+++ training.py
@@ -0,0 +1,2 @@
+# Stsrt of python training series to teach at the youth center or inmates in some capacity.  Look into making a unicor style program where inmates that have life sentences can learn to code 
+# worrking remotely later on, so they can support their families remotely.
